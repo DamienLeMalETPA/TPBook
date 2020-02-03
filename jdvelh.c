b6e8e7020626a68a500f5b09c377b8f58939caf6
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-02-03 14:28:31 +0100

    2 chemins

---
+++ jdvelh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+	int choix_chemin = 0;
+	//navigation
+	printf("La route se separe en deux, un chemin menant au château du Roi Licorne, l'autre vers la foret enchantee, ou allez-vous ?\n[1] Au Chateau du roi Licorne\n[2] A la foret enchantee");
+	scanf("%d",&choix_chemin);
+	switch (choix_chemin){
+		case 1 :
+			printf("Vous prenez le chemin en direction du Chateau du roi Licorne\n");
+			break;
+		case 2 :
+			printf("Vous prenez le chemin en direction de la foret enchantee\n");
+			break;
+	}
+	return 0;
+}
